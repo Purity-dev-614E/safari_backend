58561efb4a24866d423af1746c02531eb8cc492b
Purity <puritysang180@gmail.com>
2025-02-23 11:14:13 +0300

    modified attendanceRoute

--- routes/attendanceRoute.js
+++ routes/attendanceRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateToken, authorizeRoles } = require('../middleware/authmiddleware');
-const Attendance = require('../models/attendancemodel');
+const attendance = require('../models/attendancemodel');
 
 // Get all attendance records
 router.get('/', authenticateToken, authorizeRoles('admin', 'superadmin'),async (req, res) => {
